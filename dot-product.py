05ada595d436bce77b519796a938e7554b34485a
JacobTheRanga <jacobirving.nz@gmail.com>
2023-03-07 13:55:28 +1300

    spelling error and added more to vector-conversion.py

--- dot-product.py
+++ dot-product.py
@@ -3,20 +3,20 @@ from math import acos, sqrt, degrees
 def main():
     vectors = []
     numToDimention = ['x','y','z']
-    dimentions = int(input('How many dimentions to the vectors: '))
+    dimensions = int(input('How many dimensions to the vectors: '))
     for vectorNum in range(2):
         vector = []
-        for dimentionNum in range(dimentions):
+        for dimentionNum in range(dimensions):
             vector.append(float(input(f'Vector {vectorNum+1} [{numToDimention[dimentionNum]}]: ')))
         vectors.append(vector)
     multiModulus = 1
     for i in range(2):
         modulus = 0
-        for a in range(dimentions):
+        for a in range(dimensions):
             modulus += vectors[i][a]**2
         multiModulus *= sqrt(modulus)
     sumVector = 0
-    for i in range(dimentions):
+    for i in range(dimensions):
         timesVector = 1
         for a in range(2):
             timesVector *= vectors[a][i]
--- vector-conversion.py
+++ vector-conversion.py
@@ -1,9 +1,25 @@
-import numpy as np
 from math import *
 
+def unitVector(vector, dimensions):
+    modulus = 1
+    for i in dimensions:
+        modulus *= vector[i]**2
+    modulus = sqrt(modulus)
+
+
+def positionVector(vector, dimensions):
+    
+
 def main():
-    vectorIn = input('Input Vector: ')
     vectorType = input('Vector Type: ')
+    dimensions = int(input('How many dimensions to the vector: '))
+    vectorIn = []
+    numToDimensions = ['x','y','z']
+    for i in range(dimensions):
+        vectorIn.append(float(input(f'Vector [{numToDimensions[i]}]')))
+    if vectorType != 'unit':
+        return unitVector(vectorIn, dimensions)
+    return positionVector(vectorIn, dimensions)
 
 if __name__ == '__main__':
     main()
