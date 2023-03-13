148371c00f3a88ce565f5537b6edde630db45d1f
JacobTheRanga <jacobirving.nz@gmail.com>
2023-03-14 00:10:51 +1300

    i lost track of what i was even doing

--- vectorMaker.py
+++ vectorMaker.py
@@ -1,4 +1,17 @@
 from math import sin, cos, tan, sqrt
+from dotenv import load_dotenv
+from os import environ
+
+load_dotenv('.env')
+
+pythag = lambda b, c: sqrt(abs(c**2 - b**2))
+rounded = lambda vector: [round(i, environ.get('ROUNDING')) for i in vector]
+
+def calc(inputs, axes):
+    for plane in inputs:
+        if len((length:=list(plane['Length'].keys()))) == 2 and\
+        plane['rightAngle'][0::2] in length:
+            return pythag(length[0], length[1])  
 
 def main():
     inputs = [
@@ -12,13 +25,16 @@ def main():
                         }
                 for inputType in ['Angle', 'Length']
                 },
-                **{
-                    'rightAngle':
-                        input(f'Right Angle points on Plane {plane+1}: ').upper()
-                }
+                'rightAngle':
+                    input(f'Right Angle points on Plane {plane+1}: ').upper()
             }
         for plane in range(int(input('No. Planes: ')))
         ]
-
+    axes = [
+                input(f'Points marking Axis[{i}]: ') 
+            for i in range(int(input('No. Dimensions: ')))
+            ]
+    print(rounded(calc(inputs, axes)))
+    
 if __name__ == '__main__':
     main()
