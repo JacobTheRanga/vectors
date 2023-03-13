d142c44dda038be25b6322295da3fa34ebd45634
JacobTheRanga <jacobirving.nz@gmail.com>
2023-03-13 17:23:36 +1300

    vector conversion fix and start to list/dictionary comprehension for vectorMaker

--- vectorConversion.py
+++ vectorConversion.py
@@ -6,17 +6,14 @@ load_dotenv('.env')
 
 modulus = lambda vector: sqrt(sum([x**2 for x in vector]))
 unitCalc = lambda vector: [i/modulus(vector) for i in vector]
-changeMagnitude = lambda vector, magnitude: [unitCalc(i)*magnitude for i in vector]
+changeMagnitude = lambda vector, magnitude: [i*magnitude for i in unitCalc(vector)]
+rounded = lambda vector: [round(i, int(environ.get('ROUNDING'))) for i in vector]
 
 def main():
     vector = [float(input(f'Vector [{i}]: ')) for i in range(int(input('Vector Dimensions: ')))]
     if input('Unit vector or change magnitude: ') == 'unit':
-        result = unitCalc(vector)
-        if input('Change Magnitude aswell?[Y/N] ').lower() == 'y':
-            result = changeMagnitude(result)
-    else:
-        result = changeMagnitude(vector, float(input('Magnitude: ')))
-    print([round(i, int(environ.get('ROUNDING'))) for i in result])
+        return print(rounded(unitCalc(vector)))
+    return print(rounded(changeMagnitude(vector)))
 
 if __name__ == '__main__':
     main()
--- vectorMaker.py
+++ vectorMaker.py
@@ -1,5 +1,23 @@
 from math import sin, asin, cos, acos, tan, atan, sqrt
 
+inputs = [
+            {
+                inputType: 
+                {
+                    {
+                        input(f'{inputType} Points on Plane {plane+1}: ').upper(): 
+                            float(input(f'{inputType} {i+1} on Plane {plane+1} Value: ')) 
+                    for i in range(int(f'No. {inputType} in Plane {plane+1}: '))
+                    }
+                } 
+            for inputType in ['Angle', 'Length']
+            'rightAngle': 
+                    input(f'Right angle points on Plane {plane+1}').upper()
+            }
+        for plane in range(int(input('No. Planes: ')))
+        ]
+print(inputs)
+
 def mapInputs(plane):
     mappedInputs = {}
     mappedInputs['inputNums'] = {}
@@ -22,7 +40,7 @@ def mapInputs(plane):
 
 def trig(angles, lengths, modulus):
     
-        
+
 def pythag(lengths, modulus):
     points = lengths[0]
     for i in points:
