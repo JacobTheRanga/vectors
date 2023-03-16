456f36578a591d8eee5d07aa5697c85463d658a1
JacobTheRanga <jacobirving.nz@gmail.com>
2023-03-16 20:54:21 +1300

    command-line interface edit

--- scripts/vectorMaker.py
+++ scripts/vectorMaker.py
@@ -1,55 +1,39 @@
-# from math import sin, cos, tan, sqrt
+from math import sin, cos, tan, sqrt
 
-# knowns = lambda plane, axis, inputs, length: [
-#                                                 known
-#                                             for axisPoint in list(axis)
-#                                             for known in list(inputs[1].keys())
-#                                             if axisPoint in list(known)
-#                                             if len(known) == len([i for i in list(known) if i in list(plane)])
-#                                             if len(known) == length
-#                                             ]
-# vector = lambda inputs: [
-#                             inputs[1][axis]
-#                         if axis in inputs[1]
-#                         else 
-#                             sqrt(sum([i**2 for i in ]))
-#                         if 
-#                             [
-#                                 sqrt(sum(map(lambda x: x**2, knownAxisPlane[0])))
-#                             if len(knownAxisPlane[0]) == 2
-#                             else 
+vector = lambda inputs: [
+                            inputs[1][axis]
+                        if axis in inputs[1]
+                        else 
+                            [
+                                [
+                                    [
+                                        known
+                                    for known in [1]
+                                    if (plane[0::2] or plane[:-1] or plane[1:]) in (axis or axis[1]+axis[0])
+                                    if len(known) == types
+                                    
+                                    ]
+                                for types in [2,3]
+                                ]
+                            for plane in inputs[0]
+                            ]
+                        for axis in inputs[2]
+                        ]
 
-#                             for knownAxisPlane in
-#                                 [
-#                                     [[
-#                                         i
-#                                     for i in knowns(plane, axis, inputs, 2)
-#                                     ],
-#                                     [
-#                                         i
-#                                     for i in knowns(plane, axis, inputs, 3)
-#                                     ]]
-#                                 for plane in inputs[0]
-#                                 ]
-#                             for plane in inputs[0]
-#                             ]
-#                         for axis in inputs[2]
-#                         ]
-
-# def inputs():
-#     inputs = [
-#                 [
-#                     input(f'Right angle in plane [{plane+1}]: ').upper()
-#                 for plane in range(int(input('No. of planes: ')))
-#                 ],
-#                 {
-#                     input(f'Known Points [{numKnown+1}]: ').upper():
-#                         float(input(f'Known Value [{numKnown+1}]: '))
-#                 for numKnown in range(int(input('No. of known values: ')))
-#                 },
-#                 [
-#                     input(f'Points marking Axis[{i}]: ').upper()
-#                 for i in range(int(input('No. Dimensions: ')))
-#                 ]
-#             ]
-#     return vector(inputs)
+def inputs():
+    inputs = [
+                [
+                    input(f'Right angle in plane [{plane+1}]: ').upper()
+                for plane in range(int(input('No. of planes: ')))
+                ],
+                {
+                    input(f'Known Points [{numKnown+1}]: ').upper():
+                        float(input(f'Known Value [{numKnown+1}]: '))
+                for numKnown in range(int(input('No. of known values: ')))
+                },
+                [
+                    input(f'Points marking Axis[{i}]: ').upper()
+                for i in range(int(input('No. Dimensions: ')))
+                ]
+            ]
+    return vector(inputs)
--- vectors.py
+++ vectors.py
@@ -2,14 +2,15 @@ from scripts import *
 import argparse
 
 parser = argparse.ArgumentParser(description='Vectors Calculation Scripts')
-parser.add_argument('scripts', help='Scripts wanting to execute. Format: script1,script2,script3')
-parser.add_argument('values', nargs='?', const=True, help='Input values for the scripts')
+parser.add_argument('scripts', help='Scripts wanting to execute in accending order. Format: script1/script2/script3')
+parser.add_argument('-v', '--values', nargs='?', const=True, help='Input values for the scripts')
+parser.add_argument('-i', '--input', nargs='?', const=True, help='Use function specific input system')
 args = parser.parse_args()
 
 def main():
-    run = f"rounded.rounded({'.calc('.join(args.scripts.split(','))}.calc("
+    run = f"rounded.rounded({'.calc('.join([args.scripts.split('/')[len(args.scripts.split('/'))-i-1] for i in range(len(args.scripts.split('/')))])}.calc("
     if args.values != None:
-        return eval(f"{run}{args.values}))")
+        return eval(f"{run}{eval(input('Values: '))}))")
     return eval(f"{run[:-5]}inputs())")
 
 if __name__ == '__main__':
