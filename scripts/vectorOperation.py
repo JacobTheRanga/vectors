14b919d64faf6e4c63fa66cca595cd8bac8fa5d1
JacobTheRanga <jacobirving.nz@gmail.com>
2023-03-22 15:43:43 +1300

    quick implementation bug fixing

--- scripts/vectorOperation.py
+++ scripts/vectorOperation.py
@@ -1,20 +1,26 @@
-calc = lambda inputs, operator: [
-                                    eval(operator.join(
-                                        [
-                                            inputs[num][i]
-                                        for num in range(len(inputs))
-                                        ]
-                                    ))
-                                for i in range(3)
+"""
+Custom Vector Operation Calculator
+
+This script allows the user to use any operation
+"""
+
+calc = lambda inputs: [
+                            eval(inputs[1].join(
+                                [
+                                    str(inputs[0][num][i])
+                                for num in range(len(inputs[0]))
                                 ]
+                            ))
+                        for i in range(len(inputs[0][0]))
+                        ]
 
 def inputs():
     inputs = [
-                [
+                [[
                     input(f'Vector {num+1}[{i}]: ')
-                for i in range(3)
+                for i in range(int(input('No. Dimensions: ')))
                 ]
-            for num in range(int(input('How many vectors? ')))
+            for num in range(int(input('No. Vectors: ')))],
+            input('Operator: ')
             ]
-    operator = input('Operator: ')
-    return calc(inputs, operator)
+    return calc([inputs])
--- vectors.py
+++ vectors.py
@@ -2,7 +2,7 @@
 Vectors Command Line Tool
 
 This script enables users to run all vector scripts
-through the command line with efficiency as well
+through the command line with efficiency, as well
 as allowing the user to run scripts through
 each other's inputs in order to solve complex problems
 
@@ -70,21 +70,36 @@ or
 """
 
 from scripts import *
-import argparse
+from argparse import ArgumentParser
 from dotenv import set_key, load_dotenv
 from os.path import exists
 from sys import modules
 
-parser = argparse.ArgumentParser(description='Vectors Calculation Scripts')
-parser.add_argument('-l', '--list', action='store_const', const=True, help='List all executable scripts')
-parser.add_argument('-c', '--config', nargs='*', help='Change config file settings. (rounding=4 | rounding 4)')
-parser.add_argument('-i', '--info', nargs='?', const=True, help='Usage info for specific script')
-parser.add_argument('scripts', nargs='?', help='Scripts wanting to execute in accending order. (script1/script2/script3)')
-parser.add_argument('-v', '--values', action='store_const', const=True, help='Input values for the scripts all at once instead of using the inbuilt function dependent input systems')
+parser = ArgumentParser(description='Vectors Calculation Scripts')
+parser.add_argument('-l', '--list', 
+                    action='store_const', const=True, 
+                    help='List all executable scripts'
+                    )
+parser.add_argument('-c', '--config', 
+                    nargs='*', 
+                    help='Change config file settings. (rounding=4 | rounding 4)')
+parser.add_argument('-i', '--info', 
+                    nargs='?', const=True, 
+                    help='Usage info for specific script')
+parser.add_argument('scripts', 
+                    nargs='?', 
+                    help='Scripts wanting to execute in accending order. (script1/script2/script3)')
+parser.add_argument('-v', '--values', 
+                    action='store_const', const=True, 
+                    help='Input values for the scripts all at once instead of using the inbuilt function dependent input systems')
 args = parser.parse_args()
 
-def listScripts(list):
-    for i in [a.__name__.split('.')[1] for a in modules.values() if a.__name__.split('.')[0] == 'scripts' and a.__name__ != 'scripts']:
+def listScripts():
+    for i in [
+                a.__name__.split('.')[1] 
+            for a in modules.values() 
+            if a.__name__.split('.')[0] == 'scripts' and a.__name__ != 'scripts'
+            ]:
         print(f'    -   {i}')
     return print('Get usage info with:\n~/ vectors.py -i <script>')
 
@@ -105,8 +120,11 @@ def config(pair):
 def runScripts(scripts, values):
     if scripts == None:
         scripts = input('Scripts: ')
+    scripts, functions = scripts.split('.')
     run = f"rounded.rounded({'.calc('.join([scripts.split('/')[len(scripts.split('/'))-i-1] for i in range(len(scripts.split('/')))])}.calc("
     if values != None:
+        if len(functions):
+            run = f'{run[:-5]}{functions}('
         return eval(f"{run}{eval(input('Values: '))}))")
     return eval(f"{run[:-5]}inputs())")
 
@@ -115,7 +133,7 @@ def main():
         set_key('.env', 'ROUNDING', '4')
     load_dotenv('.env')
     if args.list != None:
-        return listScripts(args.list)
+        return listScripts()
     if args.info != None:
         return info(args.info)
     if args.config != None:
